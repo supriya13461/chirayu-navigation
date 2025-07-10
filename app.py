7ccefeef671710face3167f53fcbdb2b7efe2224
supriya13461 <supriya.s@ashokleyland.com>
2025-07-10 14:43:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -232,6 +232,8 @@ def generate_home_qr():
     return send_file(buffer, mimetype="image/png")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
 
 
